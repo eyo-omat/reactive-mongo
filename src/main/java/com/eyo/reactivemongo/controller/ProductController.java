35feb480e8b21c52320021ce186f86776ebe221d
Eyioyo Omatsola <eyo.omat@gmail.com>
2022-02-10 14:41:08 +0000

    :clown_face: Added Product Controller

---
+++ src/main/java/com/eyo/reactivemongo/controller/ProductController.java
@@ -0,0 +1,50 @@
+package com.eyo.reactivemongo.controller;
+
+import com.eyo.reactivemongo.dto.ProductDto;
+import com.eyo.reactivemongo.service.ProductService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    @NonNull
+    private ProductService productService;
+
+    @GetMapping
+    public Flux<ProductDto> getProducts() {
+        return productService.getProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ProductDto> getProductById(@PathVariable String id) {
+        return productService.getProduct(id);
+    }
+
+    @GetMapping("/product-range")
+    public Flux<ProductDto> getProductBetweenRange(@RequestParam double min, @RequestParam double max) {
+        return productService.getProductInRange(min, max);
+    }
+
+    @PostMapping
+    public Mono<ProductDto> saveProduct(@RequestBody Mono<ProductDto> productDtoMono) {
+        return productService.saveProduct(productDtoMono);
+    }
+
+    @PutMapping("/update/{id}")
+    public Mono<ProductDto> saveProduct(@RequestBody Mono<ProductDto> productDtoMono, @PathVariable String id) {
+        return productService.updateProduct(productDtoMono, id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Mono<Void> deleteProduct(@PathVariable String id) {
+        return productService.deleteProduct(id);
+    }
+
+
+}
